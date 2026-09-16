299ec5325acf89d68212dd1aed13f645ee0de8ab
Gergo Roth <gergo.roth91@gmail.com>
2026-09-16 14:53:58 +0200

    changed broadcast to be able to filter and transform the object

--- src/sync/broadcast.rs
+++ src/sync/broadcast.rs
@@ -10,48 +10,82 @@ use super::{
     usage_counter::{UsageCounter, UsageCounterWatcher},
 };
 
-#[derive(Clone)]
 struct ReceiverQueue<T> {
     queue: ArcMutex<VecDeque<T>>,
     is_stopped: ArcMutex<bool>,
     notify: Arc<Notify>,
 }
 
-crate::object_pool_index!(struct ReceiverQueueIndex);
-
-#[derive(Clone)]
-struct ReceiverQueueList<T>
-where
-    T: Clone,
-{
-    receiver_queues: ArcMutex<ObjectPool<ReceiverQueue<T>, ReceiverQueueIndex>>,
-    to_be_removed: ArcMutex<Vec<ReceiverQueueIndex>>,
+impl<T> Clone for ReceiverQueue<T> {
+    fn clone(&self) -> Self {
+        Self {
+            queue: self.queue.clone(),
+            is_stopped: self.is_stopped.clone(),
+            notify: self.notify.clone(),
+        }
+    }
 }
 
-#[derive(Clone)]
-pub struct Sender<T>
-where
-    T: Clone,
-{
-    receiver_queues: ReceiverQueueList<T>,
+struct ReceiverSubscription<T> {
+    deliver: Box<dyn Fn(&T)>,
+}
+
+crate::object_pool_index!(struct ReceiverSubscriptionIndex);
+
+struct ReceiverSubscriptionList<T> {
+    subscriptions: ArcMutex<ObjectPool<ReceiverSubscription<T>, ReceiverSubscriptionIndex>>,
+    to_be_removed: ArcMutex<Vec<ReceiverSubscriptionIndex>>,
+}
+
+impl<T> Clone for ReceiverSubscriptionList<T> {
+    fn clone(&self) -> Self {
+        Self {
+            subscriptions: self.subscriptions.clone(),
+            to_be_removed: self.to_be_removed.clone(),
+        }
+    }
+}
+
+pub struct Sender<T> {
+    receiver_subscriptions: ReceiverSubscriptionList<T>,
     usage_counter: UsageCounter,
 }
 
-pub struct Receiver<T>
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        Self {
+            receiver_subscriptions: self.receiver_subscriptions.clone(),
+            usage_counter: self.usage_counter.clone(),
+        }
+    }
+}
+
+pub struct Source<T: 'static> {
+    receiver: Receiver<T, ()>,
+}
+
+impl<T> Clone for Source<T> {
+    fn clone(&self) -> Self {
+        Self {
+            receiver: self.receiver.clone(),
+        }
+    }
+}
+
+pub struct Receiver<T, TargetType>
 where
-    T: Clone,
+    T: 'static,
+    TargetType: 'static,
 {
-    receiver_queues: ReceiverQueueList<T>,
-    queue_id: ReceiverQueueIndex,
-    queue: ReceiverQueue<T>,
+    receiver_subscriptions: ReceiverSubscriptionList<T>,
+    queue_id: ReceiverSubscriptionIndex,
+    queue: ReceiverQueue<TargetType>,
+    filter_map_fn: Arc<dyn Fn(&T) -> Option<TargetType>>,
     usage_counter_watcher: UsageCounterWatcher,
 }
 
-impl<T> ReceiverQueue<T>
-where
-    T: Clone,
-{
-    pub fn new() -> Self {
+impl<T> ReceiverQueue<T> {
+    fn new() -> Self {
         Self {
             queue: arc_mutex_new(VecDeque::new()),
             is_stopped: arc_mutex_new(false),
@@ -69,80 +103,141 @@ where
     }
 }
 
-impl<T> ReceiverQueueList<T>
-where
-    T: Clone,
-{
-    pub fn new() -> Self {
+impl<T> ReceiverSubscriptionList<T> {
+    fn new() -> Self {
         Self {
-            receiver_queues: arc_mutex_new(ObjectPool::new()),
+            subscriptions: arc_mutex_new(ObjectPool::new()),
             to_be_removed: arc_mutex_new(Vec::new()),
         }
     }
 
+    fn create_mapped_receiver<TargetType>(
+        &self,
+        usage_counter_watcher: UsageCounterWatcher,
+        filter_map_fn: Arc<dyn Fn(&T) -> Option<TargetType>>,
+    ) -> Receiver<T, TargetType>
+    where
+        T: 'static,
+        TargetType: 'static,
+    {
+        let target_queue = ReceiverQueue::<TargetType>::new();
+        let delivery_queue = target_queue.clone();
+        let delivery_filter_map_fn = filter_map_fn.clone();
+        let queue_id = self
+            .subscriptions
+            .lock()
+            .create_object(ReceiverSubscription {
+                deliver: Box::new(move |object| {
+                    if let Some(target) = delivery_filter_map_fn(object) {
+                        delivery_queue.add_object_if_not_stopped(target);
+                    }
+                }),
+            });
+        Receiver {
+            receiver_subscriptions: self.clone(),
+            queue_id,
+            queue: target_queue,
+            filter_map_fn,
+            usage_counter_watcher,
+        }
+    }
+
     fn handle_to_be_removed(&self) {
-        let mut receiver_queues_guard = self.receiver_queues.lock();
+        let mut receiver_subscriptions_guard = self.subscriptions.lock();
 
         let mut to_be_removed_guard = self.to_be_removed.lock();
         while let Some(id) = to_be_removed_guard.pop() {
-            receiver_queues_guard.release_object(id);
+            receiver_subscriptions_guard.release_object(id);
         }
     }
 }
 
-impl<T> Default for Sender<T>
-where
-    T: Clone,
-{
+impl<T> Default for Sender<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T> Sender<T>
-where
-    T: Clone,
-{
+impl<T> Sender<T> {
     pub fn new() -> Self {
         Self {
-            receiver_queues: ReceiverQueueList::new(),
+            receiver_subscriptions: ReceiverSubscriptionList::new(),
             usage_counter: UsageCounter::new(),
         }
     }
 
     pub fn send(&self, object: T) {
-        self.receiver_queues.handle_to_be_removed();
-        for queue in self.receiver_queues.receiver_queues.lock().iter() {
-            queue.add_object_if_not_stopped(object.clone());
+        self.receiver_subscriptions.handle_to_be_removed();
+        for receiver in self.receiver_subscriptions.subscriptions.lock().iter() {
+            (receiver.deliver)(&object);
         }
     }
 
-    pub fn send_directly(&self, object: T, receiver: &Receiver<T>) {
-        receiver.queue.add_object_if_not_stopped(object.clone());
+    pub fn send_directly<TargetType>(&self, object: T, receiver: &Receiver<T, TargetType>)
+    where
+        TargetType: 'static,
+    {
+        if let Some(target) = (receiver.filter_map_fn)(&object) {
+            receiver.queue.add_object_if_not_stopped(target);
+        }
     }
 
-    pub fn create_receiver(&self) -> Receiver<T> {
-        let queue = ReceiverQueue::<T>::new();
-        let queue_id = self
-            .receiver_queues
-            .receiver_queues
-            .lock()
-            .create_object(queue.clone());
-        Receiver {
-            receiver_queues: self.receiver_queues.clone(),
-            queue_id,
-            queue,
-            usage_counter_watcher: self.usage_counter.watcher(),
-        }
+    pub fn create_mapped_receiver<TargetType>(
+        &self,
+        filter_map_fn: impl Fn(&T) -> Option<TargetType> + 'static,
+    ) -> Receiver<T, TargetType>
+    where
+        T: 'static,
+        TargetType: 'static,
+    {
+        self.receiver_subscriptions
+            .create_mapped_receiver(self.usage_counter.watcher(), Arc::new(filter_map_fn))
+    }
+
+    pub fn create_source(&self) -> Source<T> {
+        let mut receiver = self.create_mapped_receiver(|_| None);
+        receiver.stop();
+        Source { receiver }
+    }
+}
+
+impl<T> Sender<T>
+where
+    T: Clone + 'static,
+{
+    pub fn create_receiver(&self) -> Receiver<T, T> {
+        self.create_mapped_receiver(|object| Some(object.clone()))
+    }
+}
+
+impl<T> Source<T> {
+    pub fn create_mapped_receiver<TargetType>(
+        &self,
+        filter_map_fn: impl Fn(&T) -> Option<TargetType> + 'static,
+    ) -> Receiver<T, TargetType> {
+        self.receiver.receiver_subscriptions.create_mapped_receiver(
+            self.receiver.usage_counter_watcher.clone(),
+            Arc::new(filter_map_fn),
+        )
+    }
+}
+
+impl<T> Source<T>
+where
+    T: Clone + 'static,
+{
+    pub fn create_receiver(&self) -> Receiver<T, T> {
+        self.create_mapped_receiver(|object| Some(object.clone()))
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct SenderDropped;
 
-impl<T> Receiver<T>
+impl<T, TargetType> Receiver<T, TargetType>
 where
-    T: Clone,
+    T: 'static,
+    TargetType: 'static,
 {
     pub fn stop(&mut self) {
         *self.queue.is_stopped.lock() = true;
@@ -152,7 +247,7 @@ where
         *self.queue.is_stopped.lock() = false;
     }
 
-    pub fn try_pop(&self) -> Result<Option<T>, SenderDropped> {
+    pub fn try_pop(&self) -> Result<Option<TargetType>, SenderDropped> {
         if let Some(object) = self.queue.queue.lock().pop_front() {
             Ok(Some(object))
         } else if self.usage_counter_watcher.is_observed_dropped() {
@@ -162,7 +257,7 @@ where
         }
     }
 
-    pub async fn pop(&self) -> Result<T, SenderDropped> {
+    pub async fn pop(&self) -> Result<TargetType, SenderDropped> {
         loop {
             if let Some(object) = self.try_pop()? {
                 break Ok(object);
@@ -171,38 +266,28 @@ where
             }
         }
     }
-
-    pub fn create_receiver(&self) -> Receiver<T> {
-        let queue = ReceiverQueue::<T>::new();
-        let queue_id = self
-            .receiver_queues
-            .receiver_queues
-            .lock()
-            .create_object(queue.clone());
-        Receiver {
-            receiver_queues: self.receiver_queues.clone(),
-            queue_id,
-            queue,
-            usage_counter_watcher: self.usage_counter_watcher.clone(),
-        }
-    }
 }
 
-impl<T> Clone for Receiver<T>
+impl<T, TargetType> Clone for Receiver<T, TargetType>
 where
-    T: Clone,
+    T: 'static,
+    TargetType: 'static,
 {
     fn clone(&self) -> Self {
-        self.create_receiver()
+        self.receiver_subscriptions.create_mapped_receiver(
+            self.usage_counter_watcher.clone(),
+            self.filter_map_fn.clone(),
+        )
     }
 }
 
-impl<T> Drop for Receiver<T>
+impl<T, TargetType> Drop for Receiver<T, TargetType>
 where
-    T: Clone,
+    T: 'static,
+    TargetType: 'static,
 {
     fn drop(&mut self) {
-        self.receiver_queues
+        self.receiver_subscriptions
             .to_be_removed
             .lock()
             .push(self.queue_id);
@@ -267,6 +352,67 @@ mod tests {
     }
 
     #[test]
+    fn send_to_mapped_receiver() {
+        let sender = Sender::<String>::new();
+        let receiver = sender.create_mapped_receiver(|object| {
+            object.parse::<usize>().ok().filter(|value| value % 2 == 0)
+        });
+
+        sender.send("1".to_string());
+        sender.send("2".to_string());
+        sender.send_directly("3".to_string(), &receiver);
+        sender.send_directly("4".to_string(), &receiver);
+
+        assert_eq!(receiver.try_pop().unwrap(), Some(2));
+        assert_eq!(receiver.try_pop().unwrap(), Some(4));
+        assert_eq!(receiver.try_pop().unwrap(), None);
+    }
+
+    #[test]
+    fn cloned_mapped_receiver_preserves_filter_map() {
+        let sender = Sender::<String>::new();
+        let receiver0 = sender
+            .create_mapped_receiver(|object| object.strip_prefix("id:").map(str::to_uppercase));
+        let receiver1 = receiver0.clone();
+
+        sender.send("id:one".to_string());
+        sender.send("ignored".to_string());
+
+        assert_eq!(receiver0.try_pop().unwrap(), Some("ONE".to_string()));
+        assert_eq!(receiver1.try_pop().unwrap(), Some("ONE".to_string()));
+        assert_eq!(receiver0.try_pop().unwrap(), None);
+        assert_eq!(receiver1.try_pop().unwrap(), None);
+    }
+
+    #[test]
+    fn mapped_receiver_stop_and_resume() {
+        let sender = Sender::<usize>::new();
+        let mut receiver = sender.create_mapped_receiver(|value| value.checked_mul(2));
+
+        sender.send(1);
+        receiver.stop();
+        sender.send(2);
+        receiver.resume();
+        sender.send(3);
+
+        assert_eq!(receiver.try_pop().unwrap(), Some(2));
+        assert_eq!(receiver.try_pop().unwrap(), Some(6));
+        assert_eq!(receiver.try_pop().unwrap(), None);
+    }
+
+    #[test]
+    fn mapped_receiver_supports_non_clone_target() {
+        struct NonCloneTarget(usize);
+
+        let sender = Sender::<usize>::new();
+        let receiver = sender.create_mapped_receiver(|value| Some(NonCloneTarget(*value)));
+
+        sender.send(7);
+
+        assert_eq!(receiver.try_pop().unwrap().unwrap().0, 7);
+    }
+
+    #[test]
     fn send_stop_send_resume_send() {
         let sender = Sender::<String>::new();
 
@@ -302,21 +448,22 @@ mod tests {
 
         sender.send("0".to_string());
 
-        assert_eq!(sender.receiver_queues.receiver_queues.lock().len(), 0);
+        assert_eq!(sender.receiver_subscriptions.subscriptions.lock().len(), 0);
     }
 
     #[tokio::test]
     async fn drop_sender() {
-        let (receiver0, receiver1) = {
+        let (receiver0, receiver1, source) = {
             let sender = Sender::<usize>::new();
-            let ret = (sender.create_receiver(), sender.create_receiver());
+            let source = sender.create_source();
+            let ret = (sender.create_receiver(), source.create_receiver(), source);
 
             sender.send(7);
 
             ret
         };
 
-        let receiver2 = receiver0.create_receiver();
+        let receiver2 = source.create_receiver();
 
         assert_eq!(receiver0.try_pop().unwrap(), Some(7));
         assert_eq!(receiver1.try_pop().unwrap(), Some(7));
